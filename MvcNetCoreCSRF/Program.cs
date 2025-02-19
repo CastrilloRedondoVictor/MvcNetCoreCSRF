b9c714024b8d0c80c352744c67e1a695d110ae22
Víctor Castrillo Redondo <castrilloredondovictor@gmail.com>
2025-02-19 14:08:26 +0100

    final commit for today

--- MvcNetCoreCSRF/Program.cs
+++ MvcNetCoreCSRF/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAntiforgery();
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDistributedMemoryCache();

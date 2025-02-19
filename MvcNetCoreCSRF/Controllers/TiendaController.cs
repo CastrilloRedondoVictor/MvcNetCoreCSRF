2cdee80b7d2406e8a838a82fd3b263917fc777fb
Víctor Castrillo Redondo <castrilloredondovictor@gmail.com>
2025-02-19 11:28:50 +0100

    Login y control de rutas

---
+++ MvcNetCoreCSRF/Controllers/TiendaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCoreCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Productos()
+        {
+            if(HttpContext.Session.GetString("username") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Productos(string direccion, string[] productos)
+        {
+            if (HttpContext.Session.GetString("username") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            }
+            TempData["DIRECCION"] = direccion;
+            TempData["PRODUCTOS"] = productos;
+            return RedirectToAction("PedidoFinal");
+        }
+
+        public IActionResult PedidoFinal()
+        {
+            if (HttpContext.Session.GetString("username") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            }
+
+            string direccion = TempData["DIRECCION"] as string;
+            string[] productos = TempData["PRODUCTOS"] as string[];
+
+            ViewData["DIRECCION"] = direccion;
+            return View(productos);
+        }
+    }
+}
